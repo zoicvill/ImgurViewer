48e4a89c0aebd51b6311726bc4c67f9ac03394bb
Admin <zoicvil@ya.ru>
2021-10-12 15:46:09 +0300

    create app

---
+++ app/src/main/java/com/example/imgurviewer/adapters/CommentAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.imgurviewer.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CommentAdapter: RecyclerView.Adapter<CommentAdapter.Holder>() {
+
+    inner class Holder(val view: Unit): RecyclerView.ViewHolder(view.root){
+  }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/example/imgurviewer/adapters/ImageAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.imgurviewer.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageAdapter: RecyclerView.Adapter<ImageAdapter.Holder>() {
+
+//    inner class Holder(): RecyclerView.ViewHolder(view.root){
+//    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
