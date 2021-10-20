0046f36ced2c3a9e1b39dd4165f323bfd69228de
Admin <zoicvil@ya.ru>
2021-10-20 21:48:58 +0300

    Добавил работу с Flow, ничего не работает, но все сделано

--- app/src/main/java/com/example/imgurviewer/model/GalleryItems.kt
+++ app/src/main/java/com/example/imgurviewer/model/GalleryItems.kt
@@ -35,20 +35,47 @@ data class GalleryItems(
 
     data class ImagesItem(
 
-        @field:SerializedName("link")
-        val link: String? = null,
-
-        @field:SerializedName("description")
-        val description: String? = null,
-
-        @field:SerializedName("section")
-        val section: Any? = null,
+        @field:SerializedName("id")
+        val id: String? = null,
 
         @field:SerializedName("title")
         val title: String? = null,
 
-        @field:SerializedName("id")
-        val id: String? = null,
+        @field:SerializedName("type")
+        val type: String? = null,
 
-        )
+        @field:SerializedName("link")
+        val link: String? = null
+
+    )
+
+    val imgList: List<ImagesItem> by lazy {
+        getNewImageList()
+    }
+
+    private fun getNewImageList(): List<ImagesItem> {
+        val newList = ArrayList<ImagesItem>()
+        data?.filter {
+            it?.link?.endsWith(".jpg") == true || it?.images?.getOrNull(0)?.link?.endsWith(".jpg") == true
+        }?.forEach { dataItem ->
+
+            var link = ""
+            if (dataItem?.link?.endsWith(".jpg") == true) {
+                dataItem.id?.let { link = it }
+            } else {
+                dataItem?.images?.getOrNull(0)?.id?.let { link = it }
+            }
+            newList.add(
+                ImagesItem(
+                    id = dataItem?.id,
+                    title = dataItem?.title,
+                    link = link
+                )
+            )
+        }
+
+        return newList
+    }
+
+
 }
